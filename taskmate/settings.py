c83217a26fb89491aa7a9e3579c4bd4c8cfcc57a
ArNaPa <130819942+ArNaPa@users.noreply.github.com>
2024-02-20 17:59:29 -0500

    Deploying on Render

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -11,6 +11,8 @@ import os
 import environ
 from pathlib import Path
 
+import whitenoise.middleware
+
 env = environ.Env()
 environ.Env.read_env()
 
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +141,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]       # Location of static files
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
